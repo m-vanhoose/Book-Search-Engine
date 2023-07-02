fc039ebd107ef8b35621db742d1de6d005ddc427
m-vanhoose <m-vanhoose@live.com>
2023-07-02 16:20:10 -0400

    added queries

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,18 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import {
-//   Container,
-//   Col,
-//   Form,
-//   Button,
-//   Card,
-//   Row
-// } from 'react-bootstrap';
-
-// import { useMutation } from '@apollo/client';
-// import { SAVE_BOOK } from '../utils/mutations';
-// import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
-// import Auth from '../utils/auth';
 import React, { useState, useEffect } from 'react';
 import {
   Container,
@@ -23,9 +8,11 @@ import {
   Row
 } from 'react-bootstrap';
 
-import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+import Auth from '../utils/auth';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -36,7 +23,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // const [saveBook, { error }] = useMutation(SAVE_BOOK);
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,6 +2,16 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
     query me {
-        
+        me {
+            _id
+            username
+            email
+            savedBooks {
+              _id
+              authors
+              description
+              title
+            }
+          }
     }
 `
